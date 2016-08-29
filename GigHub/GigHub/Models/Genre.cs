3808583cfa7908b5d1369a805187945f4ebbc351
Sean Rand <srand@nephilacapital.com>
2016-08-29 13:48:40 -0300

    Create Gig Model and DB Migration of required fields

---
+++ GigHub/GigHub/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
